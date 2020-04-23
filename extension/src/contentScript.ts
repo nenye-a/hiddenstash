9daa8953dd75c39c9d6379737d0211bf487c7607
Simon <sstur@me.com>
2020-04-23 00:58:49 -0700

    Insert DOM element on amazon product details page

--- extension/src/contentScript.ts
+++ extension/src/contentScript.ts
@@ -8,5 +8,24 @@ if (
 }
 
 function main() {
-  console.log('Hello World!');
+  let addToCartButtonContainer = document.querySelector(
+    '#addToCart_feature_div',
+  );
+  if (addToCartButtonContainer) {
+    let button = document.createElement('button');
+    button.appendChild(document.createTextNode('Click Me'));
+    button.addEventListener('click', (event) => {
+      event.preventDefault();
+      alert('Clicked');
+    });
+    // This entire next section is for inserting something after an existing element 😱
+    if (addToCartButtonContainer.nextSibling) {
+      addToCartButtonContainer.parentNode.insertBefore(
+        button,
+        addToCartButtonContainer.nextSibling,
+      );
+    } else {
+      addToCartButtonContainer.parentNode.appendChild(button);
+    }
+  }
 }
