3fa9457955cc46599329e74dbefa96050c55dad1
nathaniaelvina <nathaniaelvina@hotmail.com>
2020-04-23 12:35:44 +0700

    initial extension setup

---
+++ extension/src/contentScript.ts
@@ -0,0 +1,13 @@
+chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
+  if (msg.color) {
+    console.log('Receive color = ' + msg.color);
+    document.body.style.backgroundColor = msg.color;
+    sendResponse('Change color to ' + msg.color);
+  } else {
+    sendResponse('Color message is none.');
+  }
+});
+
+chrome.runtime.onInstalled.addListener(() => {
+  console.log('Hiddenstash installed');
+});
