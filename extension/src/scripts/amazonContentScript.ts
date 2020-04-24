df9bb920f0e0c83b62fe4bc997fecd24cf3a733f
nathaniaelvina <nathaniaelvina@hotmail.com>
2020-04-24 17:32:05 +0700

    add button on deals

--- extension/src/scripts/amazonContentScript.ts
+++ extension/src/scripts/amazonContentScript.ts
@@ -4,6 +4,19 @@ let addToCartButtonContainer = document.querySelector('#addToCart_feature_div');
 let searchResultContainer = document.querySelectorAll(
   'div.s-border-bottom>div.a-section.a-spacing-medium',
 );
+let dealContainer = document.querySelectorAll('.dealDetailContainer');
+if (dealContainer.length > 0) {
+  dealContainer.forEach((item) => {
+    let button = renderGetTodayButton({
+      style: { 'margin-bottom': '10px' },
+      onClick: () => {},
+      showIcon: false,
+      mode: 'small',
+    });
+
+    item.appendChild(button);
+  });
+}
 if (searchResultContainer.length > 0) {
   searchResultContainer.forEach((item) => {
     let button = renderGetTodayButton({
