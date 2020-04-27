f1faaf86fbe92087d5a289266e689db2e7d9d469
nathaniaelvina <nathaniaelvina@hotmail.com>
2020-04-27 13:59:53 +0700

    show and hide iframe

--- extension/src/scripts/contentScript.ts
+++ extension/src/scripts/contentScript.ts
@@ -7,4 +7,58 @@ if (
   document.addEventListener('DOMContentLoaded', () => main());
 }
 
-function main() {}
+function main() {
+  let isCartOpen = false;
+
+  let body = document.querySelector('body');
+  console.log(body, 'BODY');
+  if (body) {
+    let hiddenstashLogo = chrome.runtime.getURL('hiddenstash-icon.png');
+    let iframe = document.createElement('iframe');
+    iframe.setAttribute('id', 'hiddenstash-app');
+    iframe.setAttribute('class', 'hiddenstash-app');
+    iframe.setAttribute('src', 'https://google.com'); // TODO: change src
+    Object.assign(iframe.style, {
+      width: '410px',
+      height: '470px',
+      position: 'fixed',
+      right: '30px',
+      bottom: '30px',
+    });
+
+    let image = document.createElement('img');
+    image.setAttribute('src', hiddenstashLogo);
+    image.setAttribute('id', 'hiddenstash-icon');
+    Object.assign(image.style, {
+      'object-fit': 'contain',
+      width: '90px',
+      height: '90px',
+      position: 'fixed',
+      right: '30px',
+      bottom: '30px',
+    });
+    image.addEventListener('click', (event: MouseEvent) => {
+      event.preventDefault();
+      if (body) {
+        body.removeChild(image);
+        body.appendChild(iframe);
+        isCartOpen = true;
+      }
+    });
+
+    body.appendChild(image);
+
+    // hide iframe when clicking outside iframe
+    document.addEventListener('click', (evt: MouseEvent) => {
+      let existingIframe = document.querySelector('#hiddenstash-app');
+      // @ts-ignore
+      let notClickOnIcon = evt.target.id !== 'hiddenstash-icon';
+      if (evt.target !== existingIframe && isCartOpen && notClickOnIcon) {
+        console.log('masuk');
+        body?.removeChild(iframe);
+        body?.appendChild(image);
+        isCartOpen = false;
+      }
+    });
+  }
+}
