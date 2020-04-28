4d410ad2568d21b02b3c7ed37896ca53d553566e
RaymondAnggara <raymond@kodefox.com>
2020-04-28 15:11:46 +0700

    Add controller indexing endpoint route and type object

---
+++ backend/src/types/index.ts
@@ -0,0 +1,11 @@
+export type ResponseObject = {
+  success: boolean;
+  message: string;
+  data?: Object;
+};
+
+export type QueryResultObject = {
+  success: boolean;
+  message: string;
+  data: any;
+};
