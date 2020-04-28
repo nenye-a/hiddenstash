307e4aa6ad4722703145e04e491649a64ee37b1b
nathaniaelvina <nathaniaelvina@hotmail.com>
2020-04-28 16:17:33 +0700

    add fetching library

--- extension-app/src/App.tsx
+++ extension-app/src/App.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import { Provider as ExoflexProvider } from 'exoflex';
+import { ClientContextProvider, createClient } from 'react-fetching-library';
 
 import MainRoute from './routes/MainRoute';
 import customTheme from './constants/theme';
 
+const client = createClient({});
+
 export default function App() {
   return (
-    <ExoflexProvider theme={customTheme}>
-      <MainRoute />
-    </ExoflexProvider>
+    <ClientContextProvider client={client}>
+      <ExoflexProvider theme={customTheme}>
+        <MainRoute />
+      </ExoflexProvider>
+    </ClientContextProvider>
   );
 }
