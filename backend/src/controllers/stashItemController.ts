5a6a322c7ffb8736df3d1c93afc7f6940b1a95aa
RaymondAnggara <raymond@kodefox.com>
2020-04-28 15:09:54 +0700

    add stashItem create and get controller

---
+++ backend/src/controllers/stashItemController.ts
@@ -0,0 +1,73 @@
+import { Request, Response } from 'express';
+import jwt from 'jsonwebtoken';
+import prisma from '../prisma';
+import { SERVER_BAD_REQUEST, BAD_REQUEST, API_SECRET } from '../constants';
+
+type RequestBodyStashItem = {
+  name?: string;
+  price?: number;
+};
+
+export let getStashItemController = async (req: Request, res: Response) => {
+  let requestToken = req.headers['x-auth-token'];
+  try {
+    if (!requestToken) {
+      res.status(SERVER_BAD_REQUEST).json({
+        ...BAD_REQUEST,
+      });
+      return;
+    }
+    let payloadToken = jwt.verify(String(requestToken), API_SECRET);
+    let stashItems = await prisma.stashItem.findMany({
+      where: {
+        token: payloadToken,
+      },
+      select: { name: true, price: true, result: true },
+    });
+    res.send(stashItems);
+  } catch {
+    res.status(SERVER_BAD_REQUEST).json({
+      ...BAD_REQUEST,
+    });
+    return;
+  }
+};
+
+export let addStashItemController = async (req: Request, res: Response) => {
+  let requestToken = req.headers['x-auth-token'];
+  try {
+    if (!requestToken) {
+      res.status(SERVER_BAD_REQUEST).json({
+        ...BAD_REQUEST,
+      });
+      return;
+    }
+    let requestBody: RequestBodyStashItem = req.body;
+    if (!requestBody.name || !requestBody.price) {
+      res.status(SERVER_BAD_REQUEST).json({
+        ...BAD_REQUEST,
+      });
+      return;
+    }
+    let payloadToken = jwt.verify(String(requestToken), API_SECRET);
+    let stashItem = await prisma.stashItem.create({
+      data: {
+        name: requestBody.name,
+        price: requestBody.price,
+        token: payloadToken.toString(),
+      },
+      select: { name: true, price: true, result: true },
+    });
+    res.send(stashItem);
+  } catch {
+    res.status(SERVER_BAD_REQUEST).json({
+      ...BAD_REQUEST,
+    });
+    return;
+  }
+};
+
+export default {
+  getStashItemController,
+  addStashItemController,
+};
