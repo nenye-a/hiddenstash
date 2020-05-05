e26afc133c856d2ae972c2458b9a4b980f516503
nathaniaelvina <nathaniaelvina@hotmail.com>
2020-05-05 12:08:43 +0700

    extract product info

--- extension/src/scripts/amazonContentScript.ts
+++ extension/src/scripts/amazonContentScript.ts
@@ -1,38 +1,67 @@
 import renderGetTodayButton from '../components/GetTodayButton';
 import insertNodeAfterExistingElement from '../helpers/insertNodeAfterExistingElement';
 
-let addToCartButtonContainer = document.querySelector('#addToCart_feature_div');
+let addToCartButtonContainer = document.querySelector(
+  '#addToCart_feature_div>.a-button-stack',
+);
 let searchResultContainer = document.querySelectorAll(
   'div.s-border-bottom>div.a-section.a-spacing-medium',
 );
-let dealContainer = document.querySelectorAll('.dealDetailContainer');
+let dealContainer = document.querySelectorAll('div.dealContainer.dealTile');
+
 if (dealContainer.length > 0) {
   dealContainer.forEach((item) => {
+    let stackButtonContainer = item.querySelector('.a-row.stackToBottom');
+    let productName = item.querySelector('a#dealTitle')?.textContent;
+    let price = item.querySelector('.priceBlock>span.dealPriceText')
+      ?.textContent;
+    let source = item.querySelector('a#dealTitle')?.getAttribute('href');
+    let productImage = item.querySelector('img')?.getAttribute('src');
     let button = renderGetTodayButton({
       style: { 'margin-bottom': '10px' },
-      onClick: () => {},
-      showIcon: false,
-      mode: 'small',
+      onClick: () => {
+        // eslint-disable-next-line no-console
+        console.log(productName, price, source, productImage);
+      },
     });
 
-    item.appendChild(button);
+    stackButtonContainer?.appendChild(button);
   });
 }
 if (searchResultContainer.length > 0) {
   searchResultContainer.forEach((item) => {
+    let productName = item.querySelector('h2')?.textContent;
+    let price = item.querySelector('a>span.a-price>span.a-offscreen')
+      ?.textContent;
+    let source = item.querySelector('h2>a')?.getAttribute('href');
+    let productImage = item.querySelector('img')?.getAttribute('src');
     let button = renderGetTodayButton({
       mode: 'small',
       style: { 'margin-top': '5px' },
-      onClick: () => {},
+      onClick: () => {
+        // eslint-disable-next-line no-console
+        console.log(productName, price, source, productImage);
+      },
     });
 
     item.appendChild(button);
   });
 }
 if (addToCartButtonContainer) {
+  let productName = document
+    .querySelector('#productTitle')
+    ?.textContent?.trim();
+  let price = document.querySelector('#priceblock_ourprice')?.textContent;
+  let productImage = document
+    .querySelector('div#imgTagWrapperId>img')
+    ?.getAttribute('src');
+  let source = window.location.href;
   let button = renderGetTodayButton({
     style: { 'margin-bottom': '10px' },
-    onClick: () => {},
+    onClick: () => {
+      // eslint-disable-next-line no-console
+      console.log(productName, price, productImage, source);
+    },
   });
 
   insertNodeAfterExistingElement(button, addToCartButtonContainer);
