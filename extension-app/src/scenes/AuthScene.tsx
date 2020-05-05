9a02a60c1f741826d5fa23261b5922e2aa570b34
nathaniaelvina <nathaniaelvina@hotmail.com>
2020-05-05 18:28:54 +0700

    change store keys

--- extension-app/src/scenes/AuthScene.tsx
+++ extension-app/src/scenes/AuthScene.tsx
@@ -2,14 +2,15 @@ import React from 'react';
 import { Redirect, useParams } from 'react-router-dom';
 
 import ErrorComponent from '../components/ErrorComponent';
+import { TOKEN } from '../constants/storeKeys';
 
 type Params = { token: string };
 
 export default function AuthScene() {
   let params = useParams<Params>();
-  let hasToken = window.localStorage.getItem('hiddenstash-token');
+  let hasToken = window.localStorage.getItem(TOKEN);
   if (params.token) {
-    window.localStorage.setItem('hiddenstash-token', params.token);
+    window.localStorage.setItem(TOKEN, params.token);
     return <Redirect to="/" />;
   } else if (hasToken) {
     return <Redirect to="/" />;
--- extension-app/src/scenes/Home.tsx
+++ extension-app/src/scenes/Home.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Redirect } from 'react-router-dom';
 
+import { TOKEN } from '../constants/storeKeys';
+
 import SearchHistory from './SearchHistory';
 
 export default function Home() {
-  let hasToken = window.localStorage.getItem('hiddenstash-token');
+  let hasToken = window.localStorage.getItem(TOKEN);
   if (!hasToken) {
     return <Redirect to="/auth" />;
   }
